5a1e766e271be354852963a302d37a918a35f6ba
Batelo13 <batelo1302@hotmail.com>
2026-08-08 12:20:32 -0300

    chore: initialize Rendo Spring Boot project

---
+++ src/main/java/com/curso/gestaoinvestimentos/GestaoInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.gestaoinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoInvestimentosApplication.class, args);
+	}
+
+}
